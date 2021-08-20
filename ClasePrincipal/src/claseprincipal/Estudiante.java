9181327e84c63d036ba32368826164542ef25e72
JuanPlata321 <juan.plata.2018@upb.edu.co>
2021-08-19 23:01:53 -0500

    Segundo Avance tarea

--- ClasePrincipal/src/claseprincipal/Estudiante.java
+++ ClasePrincipal/src/claseprincipal/Estudiante.java
@@ -1,10 +1,20 @@
 
 package claseprincipal;
 
-public class Estudiante 
-{
-    private String nombre;
+//Aqui se definine la clase estudiante con su tipo de dado string en el nombre y en la eadad.
+public class Estudiante{
+    private String Nombre;
     private int edad;
-    
+
+    //Aqui se define el constructor para los datos que definimos antes.
+    public Estudiante(String n, int e) {Nombre=n;edad=e;}
+
+    //Aqui por ultimo se definen los getters los cuales nos serviran despues para el uso de esta clase.
+    public String getNombre() {
+        return Nombre;
+    }
+    public int getedad() {
+        return edad;
+    }
 }
     
--- ClasePrincipal/src/claseprincipal/Nodo.java
+++ ClasePrincipal/src/claseprincipal/Nodo.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package claseprincipal;
 
-/**
- *
- * @author plata
- */
-public class Nodo {
-    
+//Aqui se define la clase del Nodo con los datos que se nos dio en el diagrama
+//dandole al valor el tipo de dato estudiante y al siguiente el tipo de dato Nodo como se pidio en el programa.
+ public class Nodo{
+    private Estudiante valor;
+    private Nodo siguente;
+
+    //Aqui se crea el constructor para el Nodo con un parametro el cual va a ser el valor que va a contener el nodo.
+    public Nodo(Estudiante n1) {
+        valor=n1;
+        siguente=null;
+    }
+
+    //Aqui por ultimo creamos un constructor con el cual del nodo vamos a poder obtener los datos de los estudiantes
+    //solicitados los cuales serian la Edad y el Nombre de estos para al final retornar el valor de estos.
+    public String getinfo() {
+        String a="";
+        a += "Nombre: " +valor.getNombre()+"\n";
+        a+= "Edad: "+valor.getedad();
+        return a;
+    }
 }
